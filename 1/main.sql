7b5b30d4f66a0f5d5f75a4da89b77f72efabcfd2
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-02 22:46:27 -0300

    add 1

---
+++ 1/main.sql
@@ -0,0 +1,5 @@
+.mode csv input
+create table input (value int, id integer primary key autoincrement);
+.import input input
+
+SELECT SUM(input.value < next.value) FROM input JOIN input next ON input.id+1 = next.id LIMIT 20;
