9ee60e47ffe0512425a89553c1ea84640fff4b8c
Sebastian Waisbrot <seppo0010@gmail.com>
2021-12-16 01:17:46 -0300

    Add day 15 part 1

--- 15/part1.sql
+++ 15/part1.sql
@@ -1,9 +1,8 @@
 .mode csv input
-CREATE TABLE grid (value integer, x integer, y integer, id integer primary key autoincrement);
-.import miniinput grid
+CREATE TABLE grid (value varchar(100), x integer, y integer, id integer primary key autoincrement);
+.import input grid
 update grid set y = id-1;
 
-update grid set x = (SELECT MAX(id) + 1 FROM grid);
 update grid set x = 0;
 
 PRAGMA recursive_triggers = ON;
@@ -20,18 +19,48 @@ CREATE TABLE positions (x integer, y integer, value INTEGER
 	, PRIMARY KEY(x, y)
 );
 
+CREATE TABLE log(x integer, y integer, value integer);
 
-CREATE TRIGGER next_position
-AFTER INSERT ON positions WHEN NEW.x || ',' || NEW.y != (SELECT MAX(x) || ',' || MAX(y) FROM grid)
+
+-- CREATE TRIGGER next_position
+-- AFTER INSERT ON positions WHEN NEW.x || ',' || NEW.y != (SELECT MAX(x) || ',' || MAX(y) FROM grid)
+-- BEGIN
+-- INSERT INTO log VALUES (NEW.x, NEW.y, NEW.value);
+-- INSERT INTO positions SELECT NEW.x+1, NEW.y, NEW.value + grid.value FROM grid WHERE x = NEW.x + 1 AND y = NEW.y
+-- ON CONFLICT (x, y) DO UPDATE SET value = CASE WHEN excluded.value < positions.value THEN excluded.value ELSE positions.value END;
+-- INSERT INTO positions SELECT NEW.x, NEW.y+1, NEW.value + grid.value FROM grid WHERE x = NEW.x AND y = NEW.y + 1
+-- ON CONFLICT (x, y) DO UPDATE SET value = CASE WHEN excluded.value < positions.value THEN excluded.value ELSE positions.value END;
+-- END;
+
+CREATE TABLE iteration ( whole INTEGER, previous INTEGER );
+INSERT INTO iteration VALUES(0, -1);
+
+CREATE TRIGGER search_value
+AFTER UPDATE ON iteration WHEN NEW.previous != NEW.whole
 BEGIN
-INSERT INTO positions SELECT NEW.x+1, NEW.y, NEW.value + grid.value FROM grid WHERE x = NEW.x + 1 AND y = NEW.y
-ON CONFLICT (x, y) DO UPDATE SET value = CASE WHEN excluded.value < positions.value THEN excluded.value ELSE positions.value END;
-INSERT INTO positions SELECT NEW.x, NEW.y+1, NEW.value + grid.value FROM grid WHERE x = NEW.x AND y = NEW.y + 1
-ON CONFLICT (x, y) DO UPDATE SET value = CASE WHEN excluded.value < positions.value THEN excluded.value ELSE positions.value END;
+UPDATE positions SET value = (
+    SELECT MIN(value) FROM (
+        SELECT positions.value
+        UNION 
+        SELECT (
+            SELECT grid.value FROM grid WHERE grid.x = positions.x AND grid.y = positions.y
+        ) + (
+            SELECT MIN(p.value) FROM positions p WHERE
+                (p.x + 1 = positions.x AND p.y = positions.y) OR
+                (p.x - 1 = positions.x AND p.y = positions.y) OR
+                (p.x = positions.x AND p.y + 1 = positions.y) OR
+                (p.x = positions.x AND p.y - 1 = positions.y)
+        )
+    )
+);
+UPDATE iteration set previous = whole, whole = (SELECT SUM(value) FROM positions);
 END;
 
-INSERT INTO positions SELECT 0, 0, value FROM grid WHERE x = 0 AND y = 0;
+INSERT INTO positions SELECT x, y, 1234567890 FROM grid;
+UPDATE positions SET value = 0 WHERE x = 0 AND y = 0;
+UPDATE iteration SET whole = 0, previous = -1;
 
 DROP TABLE grid;
-DROP TRIGGER next_position;
+DROP TRIGGER search_value;
+SELECT value FROM positions WHERE x = (SELECT MAX(x) FROM positions) AND y = (SELECT MAX(y) FROM positions);
 .dump
